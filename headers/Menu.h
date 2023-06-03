fee2e0141be57eb18d0ebbadc22355f1229b2da2
Adriano Machado <93844395+Adriano-7@users.noreply.github.com>
2023-06-03 21:06:33 +0100

    Solved some merge conflicts

--- headers/Menu.h
+++ headers/Menu.h
@@ -8,6 +8,9 @@
 #include <limits>
 #include <chrono>
 
+using namespace std;
+using namespace std::chrono;
+
 /**
  * @class Menu
  *
@@ -15,67 +18,20 @@
  */
 class Menu {
 public:
-    /**
-     * Constructor for the Menu class.
-     *
-     * @param database The instance of the Database class used by the menu.
-     */
-    Menu(Database& database);
-
-    /**
-     * Displays the data selection menu.
-     */
+    Menu(Database& database) : database(database) {};
     void showDataSelectionMenu();
 
 private:
-    Database database; ///< The instance of the database used by the menu.
-
-    /**
-     * Displays the menu for selecting a small size graph.
-     */
+    Database database;
     void showSmallGraphMenu();
-
-    /**
-     * Displays the menu for selecting a medium/large graph.
-     */
     void showMediumGraphMenu();
-
-    /**
-     * Displays the menu for selecting an extra connected graph.
-     */
     void showExtraMenu();
-
-    /**
-     * Displays the main menu and handles user input.
-     */
     void showMainMenu();
-
-    /**
-     * Displays the menu for the Backtracking TSP algorithm and computes the shortest path.
-     */
     void showBacktrackingMenu();
-
-    /**
-     * Displays the menu for the triangular approximation heuristic and computes the shortest path.
-     */
     void showTriangularMenu();
-
-    /**
-     * Displays the menu for a special heuristic and computes the shortest path.
-     */
     void showSpecialHeuristicMenu();
-
-    /**
-     * Waits for user input.
-     */
     void waitForInput();
-
-    /**
-     * Retrieves an integer input from the user.
-     *
-     * @return The integer input.
-     */
     int getIntFromUser();
 };
 
-#endif // INC_2ND_PROJECT_MENU_H
+#endif
